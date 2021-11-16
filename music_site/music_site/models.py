d87af1f7cfd23de86ca67d3d8c0125833d9a95c9
Gipss <gissa.dmtr@gmail.com>
2021-11-16 13:04:22 +0200

    Added role model and setup changes

--- music_site/music_site/models.py
+++ music_site/music_site/models.py
@@ -7,6 +7,16 @@ class User(db.Model):
     email = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=False, nullable=False)
     image = db.Column(db.String(64), nullable=False, default='default.jpg')
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
 
     def __repr__(self):
         return f'User("{self.username}", "{self.email}")'
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32), unique=True, nullable=False)
+    users = db.relationship('User', backref='role', lazy=True)
+
+    def __repr__(self):
+        return f'Role("{self.title}")'
--- music_site/setup.py
+++ music_site/setup.py
@@ -1,10 +1,12 @@
 from music_site import app, db
-from music_site.models import User
+from music_site.models import User, Role
 
 if __name__ == '__main__':
     db.create_all()
     if not User.query.all():
-        a = User(username='admin', email='admin@admin.com', password='admin')
+        r = Role(title='Admin')
+        a = User(username='admin', email='admin@admin.com', password='admin', role=r)
+        db.session.add(r)
         db.session.add(a)
         db.session.commit()
 
