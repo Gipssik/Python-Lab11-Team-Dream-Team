0471ea343f44536593f848cbd3b5e93d872b3d8e
Gipss <gissa.dmtr@gmail.com>
2021-11-17 00:47:55 +0200

    Added register route with hashed password

--- music_site/music_site/__init__.py
+++ music_site/music_site/__init__.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 # login_manager = LoginManager(app)
 # login_manager.login_view = 'login'
 # login_manager.login_message_category = 'warning'
--- music_site/music_site/routes.py
+++ music_site/music_site/routes.py
@@ -1,9 +1,23 @@
 from flask import render_template, url_for, flash, redirect
 
-from . import app
+from . import app, bcrypt, db
 from .models import User
 
 
 @app.route('/')
-def index():
+def example():
     return 'Hello world'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # form = None
+    # if form.validate_on_submit():
+    #     hashed_password = bcrypt.generate_password_hash(form.password.data)
+    #     user = User(username=form.username.data, enail=form.email.data, password=hashed_password)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     flash('Аккаунт успішно створено!', 'success')
+    #     return redirect(url_for('login'))
+    # return render_template('register.html', title='Реєстрація', form=form)
+    ...
