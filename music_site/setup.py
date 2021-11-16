d5c6db4d3a2b6368d018b4d0d1a83b9dfff922f5
Gipss <gissa.dmtr@gmail.com>
2021-11-16 12:47:22 +0200

    Setup changes

--- music_site/setup.py
+++ music_site/setup.py
@@ -1,5 +1,11 @@
 from music_site import app, db
+from music_site.models import User
 
 if __name__ == '__main__':
     db.create_all()
+    if not User.query.all():
+        a = User(username='admin', email='admin@admin.com', password='admin')
+        db.session.add(a)
+        db.session.commit()
+
     app.run(debug=True)
