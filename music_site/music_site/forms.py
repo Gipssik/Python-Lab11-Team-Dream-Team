9375dd80b57f93c3602a2063ed4b7149f337fd30
Gipss <gissa.dmtr@gmail.com>
2021-11-19 00:34:06 +0200

    Fixed Vladik bugs

--- music_site/music_site/forms.py
+++ music_site/music_site/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 # from wtforms.fields.simple import BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from models import User
+from .models import User
 
 
 class RegistrationForm(FlaskForm):
