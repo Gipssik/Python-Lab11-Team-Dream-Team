bd0170e014c377627c57c56e3897be29bd368b20
Bogdan Dubinchenko <dubinchenkob@gmail.com>
2021-11-18 22:36:17 +0200

    ...

--- music_site/music_site/forms.py
+++ music_site/music_site/forms.py
@@ -1,5 +1,15 @@
 from flask_wtf import FlaskForm
 
 # IMPORT REQUIRED FIELDS
-# from wtforms import ...
-# from wtforms.validators import ...
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Email, DataRequired, Length
+
+
+class RegisteringUser(FlaskForm):
+    name = StringField("Ім'я: ", validators=[DataRequired()])
+    email = StringField("Email: ", validators=[Email()])
+    password = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=4, max=100)])
+    remember = BooleanField("Запам'ятати: ", default=False)
+    submit = SubmitField("Війти")
+
+
