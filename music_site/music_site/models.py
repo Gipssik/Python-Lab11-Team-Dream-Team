513af8fb8346f50838907c0d0398c0e9efe0cdb0
Gipss <gissa.dmtr@gmail.com>
2021-11-18 16:20:48 +0200

    Added authorization and user model changed

--- music_site/music_site/models.py
+++ music_site/music_site/models.py
@@ -1,8 +1,15 @@
-from . import db
+from flask_login import UserMixin
+
+from . import db, login_manager
 from datetime import datetime
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), unique=True, nullable=False)
     email = db.Column(db.String(32), unique=True, nullable=False)
@@ -50,7 +57,7 @@ class Album(db.Model):
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=False, nullable=False)
-    data_created = db.Column(db.DataTime, unique=False, nullable=False, default=datetime.utcnow)
+    data_created = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow)
     image = db.Column(db.String(64), unique=False, nullable=False, default='default.jpg')
     users = db.relationship('User', backref='group', lazy=True)
     albums = db.relationship('Album', backref='group', lazy=True)
--- music_site/music_site/routes.py
+++ music_site/music_site/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, flash, redirect
+from flask_login import login_user
 
 from . import app, bcrypt, db
 from .models import User
@@ -14,10 +15,23 @@ def register():
     # form = None
     # if form.validate_on_submit():
     #     hashed_password = bcrypt.generate_password_hash(form.password.data)
-    #     user = User(username=form.username.data, enail=form.email.data, password=hashed_password)
+    #     user = User(username=form.username.data, email=form.email.data, password=hashed_password)
     #     db.session.add(user)
     #     db.session.commit()
     #     flash('Аккаунт успішно створено!', 'success')
     #     return redirect(url_for('login'))
     # return render_template('register.html', title='Реєстрація', form=form)
+    ...
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # form = None
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.username.data).first()
+    #     if user and bcrypt.check_password_hash(user.password, form.password):
+    #         login_user(user, remember=form.remember.data)
+    #         return redirect(url_for('home'))
+    #     flash('Невдача. Перевірте логін і пароль.', 'danger')
+    # return render_template('login.html', title='Login', form=form)
     ...
