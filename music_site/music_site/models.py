faf991b50db25ff91a29f0f4c6a44ac468b08fae
KormyshRoman <kormish2003@gmail.com>
2021-11-18 15:12:09 +0200

    Added models: Song, Album and Group

--- music_site/music_site/models.py
+++ music_site/music_site/models.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,8 +7,9 @@ class User(db.Model):
     username = db.Column(db.String(16), unique=True, nullable=False)
     email = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=False, nullable=False)
-    image = db.Column(db.String(64), nullable=False, default='default.jpg')
+    image = db.Column(db.String(64), unique=False, nullable=False, default='default.jpg')
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
 
     def __repr__(self):
         return f'User("{self.username}", "{self.email}")'
@@ -20,3 +22,35 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'Role("{self.title}")'
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32), unique=False, nullable=False)
+    media = db.Column(db.String(64), unique=False, nullable=False)
+    image = db.Column(db.String(64), unique=False, nullable=False, default='default.jpg')
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
+
+    def __repr__(self):
+        return f'Song("{self.title}")'
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(64), unique=False, nullable=False)
+    data_created = db.Column(db.DataTime, unique=False, nullable=False, default=datetime.utcnow)
+    image = db.Column(db.String(64), unique=False, nullable=False, default='default.jpg')
+    songs = db.relationship('Song', backref='album', lazy=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+
+    def __repr__(self):
+        return f'Album("{self.label}")'
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=False, nullable=False)
+    data_created = db.Column(db.DataTime, unique=False, nullable=False, default=datetime.utcnow)
+    image = db.Column(db.String(64), unique=False, nullable=False, default='default.jpg')
+    users = db.relationship('User', backref='group', lazy=True)
+    albums = db.relationship('Album', backref='group', lazy=True)
+
+    def __repr__(self):
+        return f'Group("{self.name}")'
