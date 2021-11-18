4acc877eb0cc7a2c264c497e1bffa8dc92b6c654
Bogdan Dubinchenko <dubinchenkob@gmail.com>
2021-11-19 00:10:25 +0200

    Group and Album forms

--- music_site/music_site/forms.py
+++ music_site/music_site/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField, FieldList
 # from wtforms.fields.simple import BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from models import User
+from models import User, Group
 
 
 class RegistrationForm(FlaskForm):
@@ -29,5 +29,28 @@ class LoginForm(FlaskForm):
     password = PasswordField("Пароль: ", validators=[DataRequired()])
     remember = BooleanField("Запам\'ятати: ")
     submit = SubmitField("Війти")
+
+
+class GroupForm(FlaskForm):
+    name = StringField("Ім\'я групи: ", validators=[DataRequired(), Length(min=1, max=20)])
+    users = StringField("Ім\'я співака(ів): ", validators=[DataRequired(), Length(min=1, max=20)])
+    img = FileField("Фото групи: ", validators=[DataRequired()])
+    submit = SubmitField("Добавити")
+
+    def validate_group(self, name):
+        name_group = Group.query.filter_by(username=name.data).first()
+        if name_group:
+            raise ValidationError('Таке ім\'я вже існує')
+
+
+class AlbumForm(FlaskForm):
+    label = StringField("Ім\'я альбому: ", validators=[DataRequired(), Length(min=1, max=30)])
+    img = FileField("Обкладинка: ", validators=[DataRequired()])
+
+    def validate_album(self, label):
+        label_album = Group.query.filter_by(username=label.data).first()
+        if label_album:
+            raise ValidationError('Таке ім\'я вже існує')
+
 
 
